cfb15e0d5f6da062505eed8d14fe06cb539863d9
Tad Weber <tadweber92@gmail.com>
2026-09-19 13:39:18 -0700

    Add unit tests for NovelServiceImpl-corrected imports

--- src/test/java/com/infy/service/NovelServiceImplTest.java
+++ src/test/java/com/infy/service/NovelServiceImplTest.java
@@ -3,14 +3,12 @@ package com.infy.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
